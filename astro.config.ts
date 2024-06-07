5730dfb933e9f8298946cd34b3b00aecaf7b51ec
Thiwanka Dissanayaka <tc950907@gmail.com>
2024-06-07 08:39:40 +0530

    Configure actions

--- astro.config.ts
+++ astro.config.ts
@@ -7,5 +7,6 @@ export default defineConfig({
   // pages: './src/pages', // Path to Astro components, pages, and data
   // dist: './dist',       // When running `astro build`, path to final static output
   // public: './public',   // A folder of static files Astro will copy to the root. Useful for favicons, images, and other files that don’t need processing.
-  integrations: [preact()]
+  integrations: [preact()],
+  site: "https://iamtc.github.io",
 });
