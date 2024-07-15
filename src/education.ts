9db87a11fa10e99b277e3144ca3c4914bf6022e6
Thiwanka Dissanayaka <tc950907@gmail.com>
2024-07-15 22:59:17 +0530

    Added socials, skills and education

---
+++ src/education.ts
@@ -0,0 +1,11 @@
+import { Project } from "./models/Project";
+
+export const education: Project[] = [
+  {
+    title: "University of Westminster",
+    summary: `
+      BEng. Software Engineering - 1st Class Hons
+    `,
+    tags: [],
+  },
+];
---
+++ src/skills.ts
@@ -0,0 +1,27 @@
+import { Project } from "./models/Project";
+
+export const skills: Project[] = [
+  {
+    title: "",
+    summary: `
+    `,
+    tags: [
+      "Typescript",
+      "React",
+      "Angular",
+      ".NET",
+      "SQL",
+      "NoSQL",
+      "Redux",
+      "Firestore",
+      "SASS",
+      "Jest",
+      "Jasmine",
+      "Cypress",
+      "GCP",
+      "Azure",
+      "Kubernetes",
+      "Docker",
+    ],
+  },
+];
